8e548661bc53a8c46ba04d79f7fb3a80d1d453c8
rahul <rahul@test.com>
2025-06-05 09:45:49 +0000

    1

--- tina/config.ts
+++ tina/config.ts
@@ -1,7 +1,12 @@
 import { defineConfig } from "tinacms";
 
 export default defineConfig({
+  clientId: "b4a3ad44-9b40-4236-b87e-c1eb84120adf", 
+  token: "40b5eb1feb48b4759d42c7d906abc5f049e8d7e1",
+  branch: "main", // ✅ moved to root
+
   contentApiUrlOverride: "/admin/api/graphql",
+
   build: {
     publicFolder: "static",
     outputFolder: "admin",
